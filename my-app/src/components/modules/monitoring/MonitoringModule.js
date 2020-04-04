ccc84c6df32dafccc9ed38dc8fc791512f5f4d18
jamesang <jamesang717@gmail.com>
2020-04-04 00:57:28 -0400

    monitoring module

--- my-app/src/components/modules/monitoring/MonitoringModule.js
+++ my-app/src/components/modules/monitoring/MonitoringModule.js
@@ -1,18 +1,44 @@
 import React from 'react';
-import logo from '../../../twitter.svg';
-import TopicInputField from '../../TopicInputField'
-import '../../stylesheets/modules/MonitoringModule.css'
+import ChartProvider from './ChartProvider';
+import ListTopics from './ListTopics';
+import MonitorTopicInput from './MonitorTopicInput'
+import SentimentLineChart from './SentimentLineChart';
+import '../../stylesheets/modules/MonitoringModule.css';
 
-export default function MonitoringModule() {
-    return (
-        <div className="Monitoring-module">
-            <div className="Monitoring-component">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Tweet Analyzer
-                </p>
-                <TopicInputField />
-            </div>
-        </div>
-    );
+
+class MonitoringModule extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            chartData: ""
+        }
+
+        this.callbackFunction = this.callbackFunction.bind(this);
+    }
+
+
+    callbackFunction = (childData) => {
+        this.setState({message: childData})
+    }
+
+    render() {
+        return (
+            <div className="Monitoring-module">
+                <ChartProvider>
+                    <div className="Monitoring-component">
+                        <MonitorTopicInput parentCallback ={this.callbackFunction} />
+                    </div>
+                    <div className="Monitoring-component">
+                        <SentimentLineChart />
+                    </div>
+                </ChartProvider>
+                <div className="Monitoring-component">
+                    PREVIOUS TOPICS
+                    <ListTopics parentCallback ={this.callbackFunction} />
+                </div>
+            </div>            
+        )
+    }
 }
+export default MonitoringModule;
