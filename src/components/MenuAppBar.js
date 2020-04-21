d0c81660fdc8927a83a5f0de52c12659e52fa62b
jamesang <jamesang717@gmail.com>
2020-04-21 10:34:20 -0400

    clean up

--- src/components/MenuAppBar.js
+++ src/components/MenuAppBar.js
@@ -2,10 +2,7 @@ import React from 'react';
 import logo from '../twitter.svg';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = makeStyles((theme) => ({
